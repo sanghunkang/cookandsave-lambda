306b8050874e3f5288ec01d89db372c5f0b483c2
woojayjang <65854739+woojayjang@users.noreply.github.com>
2023-12-13 15:30:20 +0900

    문자열 xa 제거

--- services/recommend_recipe.py
+++ services/recommend_recipe.py
@@ -79,6 +79,7 @@ class RecipeRecommedModel:
             self.data['COOK STEP'] = self.data['COOK STEP'].apply(lambda x: [i.replace("\\",'') for i in x])
             self.data['COOK STEP'] = self.data['COOK STEP'].apply(lambda x: [i.replace("r",'') for i in x])
             self.data['COOK STEP'] = self.data['COOK STEP'].apply(lambda x: [i.replace("n",'') for i in x])
+            self.data['COOK STEP'] = self.data['COOK STEP'].apply(lambda x: [i.replace("xa0",'') for i in x])
 
             self.data['MENU THEME'] = self.data['MENU THEME'].apply(lambda x: x[1:-1].split(','))
             self.data['MENU THEME'] = self.data['MENU THEME'].apply(lambda x: [i.replace("'",'') for i in x])
